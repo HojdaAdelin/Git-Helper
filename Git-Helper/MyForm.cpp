085fe734265ad5ecafe609d0c18f7eb1e8beb18a
HojdaAdelin <adelinionut297@gmail.com>
2023-07-26 20:00:50 +0300

    Add project files.

---
+++ Git-Helper/MyForm.cpp
@@ -0,0 +1,18 @@
+#include "MyForm.h"
+
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+
+void main(array<String^>^ args) {
+
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	GitHelper::MyForm frm;
+	Application::Run(% frm);
+
+}
+
+// For cmd deletion Windows (/SUBSYSTEM:WINDOWS)
